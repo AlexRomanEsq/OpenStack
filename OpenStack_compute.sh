e966e130460112d138ad39734aa97788441d89cf
AlexRomanEsq <a_g_r_e@hotmail.com>
2016-11-12 19:20:06 -0600

    Delete OpenStack_compute.sh

--- OpenStack_compute.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash 
-yum install -y chrony
-yum install -y centos-release-openstack-liberty
-yum upgrade -y
-yum install -y python-openstackclient
-yum install -y openstack-selinux
-yum install -y openstack-nova-compute sysfsutils
-yum install -y openstack-neutron openstack-neutron-linuxbridge ebtables ipset
-
